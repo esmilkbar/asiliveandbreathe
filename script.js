252d495978431bef7fb38d424a86e05862e41480
esmilkbar <162457484+esmilkbar@users.noreply.github.com>
2024-05-18 19:29:15 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+onload = () => {
+  document.body.classList.remove("container");
+};
+
+document.getElementById('main-body').addEventListener('click', function() {
+  window.location.href = 'https://app.sane.fyi/s/10231c30-1502-11ef-9721-97a2068bcbc4?t=f280T8Hdt78dG2nFHJpK';
+});
